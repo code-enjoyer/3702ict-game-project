e8cc12be564a2948248eb1475e4c982ac92b7950
code-enjoyer <acodeenjoyer@gmail.com>
2022-08-31 13:12:19 +1000

    Added BehaviourStates, example NPC and states

---
+++ Mail Run/Assets/_Scripts/FSM/BehaviourStates/GenericPatrolState.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+namespace GGD
+{
+    public class GenericPatrolState : BehaviourState
+    {
+        public enum PatrolMethod
+        {
+            PingPong,
+            Loop
+        }
+
+        [SerializeField] private PatrolMethod _patrolMethod = PatrolMethod.PingPong;
+        [SerializeField] private Transform[] _waypoints = new Transform[1];
+        [SerializeField] private BehaviourState _idleState;
+        private int _currentWaypointIndex = 0;
+
+        protected override void OnEnter()
+        {
+            _owner.NavMeshAgent.SetDestination(_waypoints[_currentWaypointIndex].position);
+        }
+
+        public override void ExecuteUpdate(float deltaTime)
+        {
+            // TODO: Use a variable for "effective" stopping distance
+            if (_owner.NavMeshAgent.remainingDistance < 1f)
+            {
+                //switch (_patrolMethod)
+                //{
+                //    case PatrolMethod.PingPong:
+                //        // TODO
+                //        break;
+                //    case PatrolMethod.Loop:
+                //        // TODO
+                //        break;
+                //    default:
+                //        break;
+                //}
+                _currentWaypointIndex++;
+                if (_currentWaypointIndex >= _waypoints.Length)
+                {
+                    _currentWaypointIndex = 0;
+                }
+                _owner.NavMeshAgent.SetDestination(_waypoints[_currentWaypointIndex].position);
+            }
+            if (Random.value <= 0.001f)
+            {
+                Owner.SetState(_idleState);
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (_waypoints.Length == 0)
+            {
+                Debug.LogWarning("[GenericPatrolState] No transforms in the waypoints array!", gameObject);
+            }
+            else if (_waypoints.Any(x => x == null))
+            {
+                Debug.LogWarning("[GenericPatrolState] There are null transforms in the waypoints array!", gameObject);
+            }
+        }
+    }
+}
